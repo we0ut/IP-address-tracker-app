b5ef59b378ae938a9ed85bcf65b7e2f8a7630fa5
we0ut <andrej.vuckov456@gmail.com>
2024-04-11 23:29:05 +0200

    map init

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
+"use strict";
+
+const addressInputEl = document.querySelector(".s-app__address__input");
+
+const loadMap = (position) => {
+  console.log(position);
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+
+  const coords = [latitude, longitude];
+  let map = L.map("s-app__map").setView(coords, 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+};
+
+const getPosition = () => {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(loadMap, function () {
+      alert("Could not get your position");
+    });
+};
+getPosition();
